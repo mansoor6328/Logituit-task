c1811e01c0f4dd8dd5ae9e230d2d731e052e0fe6
mansoor6328 <mansoor6328@gmail.com>
2022-01-07 16:32:49 +0530

    Task 2

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,41 @@
+/* eslint-disable jsx-a11y/alt-text */
 import MotionHoc from "./MotionHoc";
+import first from './Images/first.jpg'
+import second from './Images/second.jpg'
+import third from './Images/third.jpg'
+import fourth from './Images/fourth.jpg'
+import fifth from './Images/fifth.jpg'
+import sixth from './Images/sixth.jpg'
+import seventh from './Images/seventh.jpg'
+import Carousel from "react-elastic-carousel";
+import './Home.css'
 
-const HomeComponent = () => {
-  // eslint-disable-next-line jsx-a11y/heading-has-content
-  return <h1></h1>;
-};
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
+
+function HomeComponent() {
+  return (
+    <>
+      {/* <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1> */}
+      <div className="HomeApp">
+        <Carousel breakPoints={breakPoints}>
+          <img className="img" src={first}></img>
+          <img className="img" src={second}></img>
+          <img className="img" src={third}></img>
+          <img className="img" src={fourth}></img>
+          <img className="img" src={fifth}></img>
+          <img className="img" src={sixth}></img>
+          <img className="img" src={seventh}></img>
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
 
 const Home = MotionHoc(HomeComponent);
 
